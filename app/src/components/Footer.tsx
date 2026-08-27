15ee1c349e4052aee22bbef98e04047bf7de7359
Jose <josemoyano059@gmail.com>
2026-08-26 21:21:15 -0300

    chore(release): v1.0.0 preparacion de metadatos de produccion

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer: React.FC = () => {
   return (
     <footer>
-      ANEXO II MIC-PG-11-01 &middot; Rev. 2 &middot; Abril 2026
+      ANEXO II MIC-PG-11-01 &middot; Rev. 2 &middot; Abril 2026 &middot; v1.0.0
     </footer>
   );
 };
