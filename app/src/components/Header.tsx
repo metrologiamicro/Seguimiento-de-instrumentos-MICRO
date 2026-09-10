8195b1ea7e8c3ef216c12271bed407a72c7eb75b
metrologiamicro <metrologiamicro@gmail.com>
2026-09-10 09:23:28 -0300

    Comment out sync button in Header component

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -28,7 +28,7 @@ export const Header: React.FC<HeaderProps> = ({
       </div>
 
       <div className="hdr-right">
-        {onSync && (
+        {/*onSync && (
           <button
             className={`hdr-btn sync-btn ${isSyncing ? "spinning" : ""}`}
             onClick={onSync}
@@ -39,7 +39,7 @@ export const Header: React.FC<HeaderProps> = ({
               <path d="M21.5 2v6h-6M2.5 22v-6h6M2 11.5a10 10 0 0 1 18.8-4.3M22 12.5a10 10 0 0 1-18.8 4.2" />
             </svg>
           </button>
-        )}
+        )*/}
 
         <button
           className="hdr-btn theme-toggle-btn"
